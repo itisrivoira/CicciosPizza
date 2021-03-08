57245029f77ef78157ae0dfee96ffd7e0d924c08
Alessandro Lopez <58600501+Bresh420@users.noreply.github.com>
2021-03-08 21:37:31 +0100

    Sito web
    Aggiunto pagine Contattaci e Login.
    Aggiunto footer delle pagine.

---
+++ Sito Web Ciccio's Pizza/Sito Web/js/superslide.js
@@ -0,0 +1,12 @@
+(function($) {
+
+	$('#slides').superslides({
+		inherit_width_from: '.cover-slides',
+		inherit_height_from: '.cover-slides',
+		play: 5000,
+		animation: 'fade',
+	});
+	
+	$( ".cover-slides ul li" ).append( "<div class='overlay-background'></div>" );
+		
+}(jQuery));
